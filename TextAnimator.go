b40fc55cd03a6f5bab0938509b302b3f35265d83
Vladyslav <v.chabanenko@autodoc.eu>
2023-06-06 17:57:55 +0300

    Fix the problem with the last symbol in a row

--- TextAnimator.go
+++ TextAnimator.go
@@ -16,9 +16,18 @@ func (t TextAnimator) PrintSequential() {
 	var str string
 	var preloadCounter int
 
-	for _, char := range t.text {
+	for i, char := range t.text {
 		str += string(char)
-		fmt.Printf("\r%s%c", str, t.preloaderParts[preloadCounter])
+
+		if i+1 != len(t.text) && t.text[i+1] == 10 {
+			fmt.Printf("\r%s", str)
+			str = "\r"
+		} else if i+1 == len(t.text) {
+			fmt.Printf("\r%s", str)
+		} else {
+			fmt.Printf("\r%s%c", str, t.preloaderParts[preloadCounter])
+		}
+
 		if char == 10 {
 			str = "\r"
 		}
