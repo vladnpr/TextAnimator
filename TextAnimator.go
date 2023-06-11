8d65811a65e720f70ecf4b2549e40536f393ed71
Vladyslav <v.chabanenko@autodoc.eu>
2023-06-12 00:27:43 +0300

    text changes

--- TextAnimator.go
+++ TextAnimator.go
@@ -6,23 +6,22 @@ import (
 )
 
 type TextAnimator struct {
-	text           string
 	preloaderParts string
 	textTime       time.Duration
 }
 
-func (t TextAnimator) PrintSequential() {
+func (t TextAnimator) PrintSequential(text string) {
 
 	var str string
 	var preloadCounter int
 
-	for i, char := range t.text {
+	for i, char := range text {
 		str += string(char)
 
-		if i+1 != len(t.text) && t.text[i+1] == 10 {
+		if i+1 != len(text) && text[i+1] == 10 {
 			fmt.Printf("\r%s", str)
 			str = "\r"
-		} else if i+1 == len(t.text) {
+		} else if i+1 == len(text) {
 			fmt.Printf("\r%s", str)
 		} else {
 			fmt.Printf("\r%s%c", str, t.preloaderParts[preloadCounter])
@@ -41,13 +40,12 @@ func (t TextAnimator) PrintSequential() {
 	}
 }
 
-func NewTextAnimator(text string) TextAnimator {
+func NewTextAnimator(textTime time.Duration) TextAnimator {
 	preloadParts := "-\\|/"
 
 	t := TextAnimator{
-		text:           text,
 		preloaderParts: preloadParts,
-		textTime:       75 * time.Millisecond,
+		textTime:       textTime,
 	}
 
 	return t
